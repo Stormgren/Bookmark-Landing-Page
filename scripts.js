5088c09fb9c81046690ba8f61506f4974cd6dbf1
Stormgren <sbnv93@gmail.com>
2019-06-25 15:00:31 +0200

    Added jQuery for sidenav functionality on smaller screens

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//   });
-
-var instance = M.Tabs.init(el, options);
-instance.select('tab_id');
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+})
