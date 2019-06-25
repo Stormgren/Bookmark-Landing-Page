58ab598dfa28bb0d45d4a038445ca0243f4366f4
Stormgren <sbnv93@gmail.com>
2019-06-25 11:41:56 +0200

    Added features part of the page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+var instance = M.Tabs.init(el, options);
+instance.select('tab_id');
